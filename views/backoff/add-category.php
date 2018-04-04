bf33dfdab5e11b2855d05860f4e541d717740503
Lythium <lythium.game@gmail.com>
2018-04-04 23:35:28 +0200

    update view

--- views/backoff/add-category.php
+++ views/backoff/add-category.php
@@ -79,12 +79,48 @@
                                         <form  action="<?= admin_url('admin.php') ?>" method="GET">
                                             <input type="hidden" name="page" value="lythrankingaddcategory">
                                             <input type="hidden" name="id_category" value="<?php echo $obj->id_category ?>">
-                                            <button id="delete_btn" type="submit" class="btn btn-info" name="button" value="delete"></i><span class="icon_text">Delete</span></button>
-                                            <br>
-                                            <button id="update_btn" type="submit" class="btn btn-info" name="button" value="update"></i><span class="icon_text">Update</span></button>
+                                            <div class="btn-group">
+                                                <button id="update_btn" type="submit" class="btn btn-info" name="button" value="update"></i><span class="icon_text">Update</span></button>
+                                                <button class="btn-drop" type="button" name="button"><i class="icon-down-open"></i></button>
+                                                <div class="dropdown">
+                                                    <button id="delete_btn" type="submit" class="btn btn-info" name="button" value="delete"></i><span class="icon_text">Delete</span></button>
+                                                </div>
+                                            </div>
                                         </form>
                                     </th>
                                 </tr>
+                                <?php $childrens = LythRankingCore::getChildrenCategory((int)$obj->id_category); ?>
+                                <?php if ($childrens): ?>
+                                    <?php foreach ($childrens as $child): ?>
+                                        <tr class="children">
+                                            <th>
+                                                <span><?php echo $child->id_category ?></span>
+                                            </th>
+                                            <th>
+                                                <span><i class="icon-level-down"></i><?php echo $child->name ?></span>
+                                            </th>
+                                            <th>
+                                                <span><?php echo $obj->name ?></span>
+                                            </th>
+                                            <th>
+                                                <span><?php echo $child->position ?></span>
+                                            </th>
+                                            <th>
+                                                <form  action="<?= admin_url('admin.php') ?>" method="GET">
+                                                    <input type="hidden" name="page" value="lythrankingaddcategory">
+                                                    <input type="hidden" name="id_category" value="<?php echo $child->id_category ?>">
+                                                    <div class="btn-group">
+                                                        <button id="update_btn" type="submit" class="btn btn-info" name="button" value="update"></i><span class="icon_text">Update</span></button>
+                                                        <button class="btn-drop" type="button" name="button"><i class="icon-down-open"></i></button>
+                                                        <div class="dropdown">
+                                                            <button id="delete_btn" type="submit" class="btn btn-info" name="button" value="delete"></i><span class="icon_text">Delete</span></button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </th>
+                                        </tr>
+                                    <?php endforeach;?>
+                                <?php endif; ?>
                             <?php endforeach;?>
                         <?php else: ?>
                             <tr class="no-items">
