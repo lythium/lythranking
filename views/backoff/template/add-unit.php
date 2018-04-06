c3ff0d860f638dad657badf93d08ff08e29eeff6
Lythium <lythium.game@gmail.com>
2018-04-07 01:29:53 +0200

    add include template

---
+++ views/backoff/template/add-unit.php
@@ -0,0 +1,24 @@
+<table class="wp-list-table widefat fixed striped pages">
+    <thead>
+        <tr>
+            <th scope="col" id="field_0" class="manage-column column-cb id-column">ID</th>
+            <th scope="col" id="field_1" class="manage-column column-title column-primary column-fields">Unit Rank</th>
+            <th scope="col" id="field_2" class="manage-column column-fields">Unit Name</th>
+            <th scope="col" id="field_3" class="manage-column column-fields">Category</th>
+            <th scope="col" id="field_4" class="manage-column column-fields">Image unit</th>
+            <th scope="col" id="field_5" class="manage-column column-fields">Positive</th>
+            <th scope="col" id="field_6" class="manage-column column-fields">Negative</th>
+            <th scope="col" id="field_7" class="manage-column column-fields">Action<i id="close_form_unit" class="icon-cancel-circled"></i></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr class="no-items">
+            <td class="colspanchange" colspan="8"> </td>
+        </tr>
+    </tfoot>
+</table>
---
+++ views/backoff/template/list-category.php
@@ -0,0 +1,68 @@
+<?php $objects = LythRankingCore::getCategoryParent(); ?>
+<?php if ($objects): ?>
+    <?php foreach ($objects as $obj): ?>
+        <tr>
+            <th>
+                <span><?php echo $obj->id_category ?></span>
+            </th>
+            <th>
+                <span><?php echo $obj->name ?></span>
+            </th>
+            <th>
+                <span></span>
+            </th>
+            <th>
+                <span><?php echo $obj->position ?></span>
+            </th>
+            <th>
+                <form  action="<?= admin_url('admin.php') ?>" method="GET">
+                    <input type="hidden" name="page" value="lythrankingaddcategory">
+                    <input type="hidden" name="id_category" value="<?php echo $obj->id_category ?>">
+                    <div class="btn-group">
+                        <button id="update_btn" type="submit" class="btn btn-info" name="button" value="update"></i><span class="icon_text">Update</span></button>
+                        <button class="btn-drop" type="button" name="button"><i class="icon-down-open"></i></button>
+                        <div class="dropdown">
+                            <button id="delete_btn" type="submit" class="btn btn-info" name="button" value="delete"></i><span class="icon_text">Delete</span></button>
+                        </div>
+                    </div>
+                </form>
+            </th>
+        </tr>
+        <?php $childrens = LythRankingCore::getChildrenCategory((int)$obj->id_category); ?>
+        <?php if ($childrens): ?>
+            <?php foreach ($childrens as $child): ?>
+                <tr class="children">
+                    <th>
+                        <span><?php echo $child->id_category ?></span>
+                    </th>
+                    <th>
+                        <span><i class="icon-level-up"></i><?php echo $child->name ?></span>
+                    </th>
+                    <th>
+                        <span><i class="icon-level-up"></i><?php echo $obj->name ?></span>
+                    </th>
+                    <th>
+                        <span><i class="icon-level-up"></i><?php echo $child->position ?></span>
+                    </th>
+                    <th>
+                        <form  action="<?= admin_url('admin.php') ?>" method="GET">
+                            <input type="hidden" name="page" value="lythrankingaddcategory">
+                            <input type="hidden" name="id_category" value="<?php echo $child->id_category ?>">
+                            <div class="btn-group">
+                                <button id="update_btn" type="submit" class="btn btn-info" name="button" value="update"></i><span class="icon_text">Update</span></button>
+                                <button class="btn-drop" type="button" name="button"><i class="icon-down-open"></i></button>
+                                <div class="dropdown">
+                                    <button id="delete_btn" type="submit" class="btn btn-info" name="button" value="delete"></i><span class="icon_text">Delete</span></button>
+                                </div>
+                            </div>
+                        </form>
+                    </th>
+                </tr>
+            <?php endforeach;?>
+        <?php endif; ?>
+    <?php endforeach;?>
+<?php else: ?>
+    <tr class="no-items">
+        <td class="colspanchange" colspan="5">Sorry, No rank Category</td>
+    </tr>
+<?php endif; ?>
