06d1c5b3c855530f769cf3652ee1336eba9ae7d2
Lythium <lythium.game@gmail.com>
2018-04-08 11:03:34 +0200

    update js

--- views/js/lr-ajax.js
+++ views/js/lr-ajax.js
@@ -1,6 +1,6 @@
 jQuery(document).ready( function($){
     // Page Add Category
-    var $formAddCategory = $('#add-category'),
+    var $formAddCategory = $('#add-category-form'),
         $closeMessage = $('.alert .icon-cancel-circled'),
         $success = $('#lythranking_success p'),
         $error = $('#lythranking_error p'),
@@ -25,7 +25,7 @@ jQuery(document).ready( function($){
 
         var $data = $(this).serializeArray();
         $data.push(
-            {name: 'action', value: 'add_lr_Process'}
+            {name: 'action', value: 'category_lr_Process'}
         );
         console.log($data);
 
@@ -74,9 +74,52 @@ jQuery(document).ready( function($){
         }
     }
     // Page list
-    var $btnDisplayForm = $('button#form_unit'),
+    var $formAddUnit = $('#add-unit-form'),
+        $btnDisplayForm = $('button#form_unit'),
+        $addDetails = $('#icone-add-row'),
         $btnCloseDisplayForm = $('#close_form_unit');
 
+    // Ajax add unit
+    $formAddUnit.submit(function(event){
+        event.preventDefault();
+        $success.parent().css('display', 'none');
+        $error.parent().css('display', 'none');
+
+        var $data = $(this).serializeArray();
+        $data.push(
+            {name: 'action', value: 'unit_lr_Process'}
+        );
+        console.log($data);
+
+        toggleSpin(true);
+        setTimeout(function(){
+            $.ajax({
+                url: ajax_object.ajaxurl,
+                type: 'POST',
+                dataType: 'JSON',
+                data: $data,
+                success: function(data){
+                    if (data.return) {
+                        console.log(data.message);
+                        $success.html(data.message);
+                        $success.parent().css('display', 'block');
+                        setTimeout(function(){
+                            $(location).attr('href', $url_page_add);
+                        }, 1500);
+                    } else {
+                        console.log(data.error);
+                        $error.html(data.error);
+                        $error.parent().css('display', 'block');
+                    };
+                    toggleSpin(false);
+                },
+                error: function(XHR, textStatus, errorThrown) {
+                    console.log(textStatus, errorThrown);
+                    toggleSpin(false);
+                }
+            });
+        }, 2000);
+    });
     $btnDisplayForm.on('click', function(event){
         event.stopPropagation();
         $(this).slideToggle(0);
@@ -86,5 +129,51 @@ jQuery(document).ready( function($){
         event.stopPropagation();
         $('#table_add_unit').slideToggle(0);
         $btnDisplayForm.slideToggle(250);
+    });
+
+    $('#upload-btn').click(function(e) {
+        e.preventDefault();
+        var image = wp.media({
+            title: 'Upload Image',
+            multiple: false
+        }).open()
+        .on('select', function(e){
+
+            var $upload_image = image.state().get('selection').first();
+
+            console.log($upload_image);
+            var $image_url = $upload_image.toJSON().url;
+
+            $('#image_url').val($image_url);
+            $('#upload_image').attr('src', $image_url);
+            $('.image_group').css('display', 'flex');
+            $('#image_url_group').css('display', 'none');
+        });
+    });
+    $('#close_upload').click(function(e) {
+        e.preventDefault();
+        $('#image_url').val('');
+        $('#upload_image').attr('src', '');
+        $('.image_group').css('display', 'none');
+        $('#image_url_group').css('display', 'block');
+    });
+
+    $addDetails.on('click', function(event){
+        event.stopPropagation();
+        $('<tr>'+
+            '<td colspan="1"></td>'+
+            '<td colspan="3">'+
+                '<input type="text"  name="positive_details[]" value=""></input>'+
+            '</td>'+
+            '<td colspan="3">'+
+                '<input type="text"  name="negative_details[]" value=""></input>'+
+            '</td>'+
+            '<td colspan="1"><i class="close-row icon-cancel-circled"></i></td>'+
+        '</tr>').insertBefore("tr.add-row");
+        $('.close-row').on('click', function(event){
+            event.preventDefault();
+            event.stopPropagation();
+            $(this).parent().parent('tr').remove();
+        });
     });
 });
