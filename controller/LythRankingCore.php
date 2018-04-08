a17471088f44a5da3196dd0fb0f5fbef93fda53c
Lythium <lythium.game@gmail.com>
2018-04-08 11:02:09 +0200

    update ajax process and add validate add unit

--- controller/LythRankingCore.php
+++ controller/LythRankingCore.php
@@ -7,16 +7,20 @@ class LythRankingCore
 
     function __construct()
     {
-        add_action( 'admin_enqueue_scripts', array($this,'load_wp_media_files' ));
+        if (isset($_GET['page'])) {
+            if ($_GET['page'] == 'lythrankinglist' || $_GET['page'] == 'lythrankingaddcategory') {
+                add_action( 'admin_enqueue_scripts', array($this,'load_wp_media_files' ));
+            }
+        }
 
-        add_action( 'wp_ajax_add_lr_Process', array($this, 'add_lr_Process' ));
-        add_action( 'wp_ajax_nopriv_add_lr_Process', array($this, 'add_lr_Process' ));
+        add_action( 'wp_ajax_category_lr_Process', array($this, 'category_lr_Process' ));
+        add_action( 'wp_ajax_nopriv_category_lr_Process', array($this, 'category_lr_Process' ));
 
-        add_action( 'wp_ajax_update_lr_Process', array($this, 'update_lr_Process' ));
-        add_action( 'wp_ajax_nopriv_update_lr_Process', array($this, 'update_lr_Process' ));
+        add_action( 'wp_ajax_unit_lr_Process', array($this, 'unit_lr_Process' ));
+        add_action( 'wp_ajax_nopriv_unit_lr_Process', array($this, 'unit_lr_Process' ));
     }
 
-    public static function add_lr_Process() {
+    public static function category_lr_Process() {
         if (!isset($_POST['name']) || empty($_POST['name']) || !LythTools::isGenericName($_POST['name'])) {
             die(json_encode(array(
                 'return' => false,
@@ -83,8 +87,55 @@ class LythRankingCore
         }
     }
 
-    public static function update_lr_Process() {
-
+    public static function unit_lr_Process() {
+        if (!isset($_POST['unit_rank']) || empty($_POST['unit_rank']) || !LythTools::isInt((int) $_POST['unit_rank'])) {
+            die(json_encode(array(
+                'return' => false,
+                'error' => 'Rank invalid'
+            )));
+        }
+        if (!isset($_POST['unit_name']) || empty($_POST['unit_name']) || !LythTools::isGenericName($_POST['unit_name'])) {
+            die(json_encode(array(
+                'return' => false,
+                'error' => 'Unit Name invalid'
+            )));
+        }
+        if (!empty($_POST['url_post'])) {
+             if (!LythTools::isUrl($_POST['url_post'])) {
+                die(json_encode(array(
+                    'return' => false,
+                    'error' => 'Url Post invalid'
+                )));
+            }
+        }
+        if (!isset($_POST['category']) || $_POST['category'] === '0' || !LythTools::isInt((int) $_POST['category'])) {
+            die(json_encode(array(
+                'return' => false,
+                'error' => 'Category invalid'
+            )));
+        }
+        if (!isset($_POST['image_url']) || empty($_POST['image_url']) || !LythTools::isUrl($_POST['image_url'])) {
+            die(json_encode(array(
+                'return' => false,
+                'error' => 'image invalid'
+            )));
+        }
+        if (!empty($_POST['positive_details'])) {
+             if (!LythTools::isString($_POST['positive_details'])) {
+                die(json_encode(array(
+                    'return' => false,
+                    'error' => 'Positive Details invalid'
+                )));
+            }
+        }
+        if (!empty($_POST['negative_details'])) {
+             if (!LythTools::isString($_POST['negative_details'])) {
+                die(json_encode(array(
+                    'return' => false,
+                    'error' => 'Negative Details invalid'
+                )));
+            }
+        }
     }
 
     public static function getItem($id_unit)
