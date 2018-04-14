d871451c4a01edc0dca4b2b719d6a776830c42f6
Lythium <lythium.game@gmail.com>
2018-04-15 01:53:10 +0200

    extern list tpl

--- views/backoff/list-ranking.php
+++ views/backoff/list-ranking.php
@@ -61,34 +61,9 @@
 
     <section id="section-3">
         <h3>List Ranking</h3>
-        <table id="lythranking-list" class="wp-list-table widefat fixed striped pages">
-            <thead>
-                <tr>
-                    <th scope="col" id="field_0" class="manage-column column-cb id-column">ID</th>
-                    <th scope="col" id="field_1" class="manage-column column-title column-primary column-fields">Unit Rank</th>
-                    <th scope="col" id="field_2" class="manage-column column-fields">Unit Name</th>
-                    <th scope="col" id="field_3" class="manage-column column-fields">Category</th>
-                    <th scope="col" id="field_4" class="manage-column column-fields">Image unit</th>
-                    <th scope="col" id="field_5" class="manage-column column-fields">Positive</th>
-                    <th scope="col" id="field_6" class="manage-column column-fields">Negative</th>
-                    <th scope="col" id="field_7" class="manage-column column-fields">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $results = LythRankingCore::getListRank(); ?>
-                <?php if ($results): ?>
-                <?php else: ?>
-                    <tr class="no-items">
-                        <td class="colspanchange" colspan="8">Sorry, No ranking</td>
-                    </tr>
-                <?php endif; ?>
-            </tbody>
-            <tfoot>
-                <tr class="no-items">
-                    <td class="colspanchange" colspan="8"> </td>
-                </tr>
-            </tfoot>
-        </table>
+        <div id="table_list_unit">
+            <?php include_once plugin_dir_path(__FILE__).'template/list-unit.php'; ?>
+        </div>
     </section>
 
 </div>
