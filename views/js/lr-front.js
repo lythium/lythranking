df398885cad5c36da4cbbdda47d50d8d71ebd08c
Lythium <lythium.game@gmail.com>
2018-04-24 09:49:29 +0200

    move icon top top active li child

--- views/js/lr-front.js
+++ views/js/lr-front.js
@@ -1,3 +1,36 @@
 jQuery(document).ready(function($){
+    var $mainNav = $('.lr-nav li'),
+        $subNav = $('.lr-sub-nav li');
 
+    $mainNav.on('click', function(){
+        var $id = $(this).attr('id'),
+            $thisSection = $('.lythranking section#'+$id),
+            $childrenId = $('.lythranking section#'+$id+' .lr-sub-nav li:first-child').attr('id');
+        $mainNav.removeClass('active-menu');
+        $(this).addClass('active-menu');
+        $('.lythranking section').removeClass('active');
+        $thisSection.addClass('active');
+        $('.lr-sub-nav li#'+$childrenId).click();
+    });
+    $subNav.on('click', function(){
+        var $id = $(this).attr('id'),
+            $thisContent = $('.lythranking .lr-content #'+$id);
+        $subNav.removeClass('active-menu');
+        $(this).addClass('active-menu');
+        $('.lythranking .lr-content div').removeClass('active');
+        $thisContent.addClass('active');
+        iconPos($(this));
+    })
+
+    var $firstChildActive = $('.lr-sub-nav li.active-menu');
+
+    function iconPos($childActive) {
+        var $childIcon = $('section.active .icon-section'),
+            $childWidth = $childActive.width(),
+            $indexInList = $('section.active .lr-sub-nav li').index($childActive);
+
+        $childIcon.css({'left': (($childWidth/2 - 23) + ($childWidth * $indexInList))+'px'})
+    }
+
+    iconPos($firstChildActive);
 });
